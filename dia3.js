9fe80bf632e6d4f5fc065fb0465047085ce366e3
HCZ369 <hugo.carreras37@gmail.com>
2023-12-21 10:48:39 -0300

    update

--- dia3.js
+++
@@ -1,7 +0,0 @@
-function findNaughtyStep(original, modified) {
-
-    const [lessWords, mostWords] =
-      [original, modified].sort((a, b) => a.length - b.length)
-  
-    return [...mostWords].find((x, i) => lessWords[i] != x) ?? "";
-  }
